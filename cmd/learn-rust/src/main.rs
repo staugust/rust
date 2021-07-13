a6fc0e8735f5cf3a5f6c22b3c5a609c8ac62d6f2
Augusto Yao <727004611@qq.com>
2021-07-13 22:30:05 +0800

    fix most simple parts

--- cmd/learn-rust/src/main.rs
+++ cmd/learn-rust/src/main.rs
@@ -8,11 +8,12 @@ struct Car {
     roof: bool,
     age: (String, u32),
 }
+
 // Get the car quality by testing the value of the input argument
 // - miles (u32)
 // Create a tuple for the car quality with the age ("New" or "Used") and miles
 // Return a tuple with the arrow `->` syntax
-fn car_quality (miles: u32) -> (String, u32) {
+fn car_quality(miles: u32) -> (String, u32) {
 
     // Declare and initialize the return tuple value
     // For a new car, set the miles to 0
@@ -27,7 +28,7 @@ fn car_quality (miles: u32) -> (String, u32) {
     }
 
     // TO DO: Return the completed tuple
-    return quality
+    return quality;
 }
 
 // Build a new "Car" using the values of four input arguments
@@ -47,21 +48,28 @@ fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Ca
         color,
         motor,
         roof,
-        age: car_quality(miles)
+        age: car_quality(miles),
     };
 
     // Return new instance of "Car" struct
-    return car
+    return car;
 }
 
 
 #[derive(PartialEq, Debug)]
 // Declare enum for Car transmission type
 enum Transmission { Manual, SemiAuto, Automatic }
+
+
 fn main() {
+    use std::collections::HashMap;
+    let mut orders: HashMap<String, u32> = HashMap::new();
+    let (mut new_cars, mut used_cars) = (1, 1);
+    let (mut manual, mut auto) = (1, 1);
+
     // Create car color array
     // TO DO: Set the values: 0 = Blue, 1 = Green, 2 = Red, 3 = Silver
-    let colors =["Blue", "Green", "Red", "Silver"];
+    let colors = ["Blue", "Green", "Red", "Silver"];
     // Initialize variables
     let (mut index, mut order) = (1, 1);
 
@@ -69,29 +77,49 @@ fn main() {
     // TO DO: Create "car" as a "Car" struct
     // TO DO: Create "engine" as a "Transmission" enum
     // TO DO: Initialize "roof" to the value when the car has a hard top
-    let mut car:Car;
+    let mut car: Car;
     let mut miles = 1000; // Start used cars with 1,000 miles
-    let roof : bool = false;  // convertible = false | hard top = true
-    let mut engine : Transmission;
+    let mut roof: bool = true;  // convertible = false | hard top = true
+    let mut engine: Transmission;
 
-    loop {
-        if order > 11 {
-            break
-        }
+    while order <= 11 {
         // Set car transmission type
-        engine = Transmission::Manual;
+        if order % 3 == 0 {
+            engine = Transmission::Automatic;
+            orders.insert("Automatic".to_string(), auto);
+            auto += 1;
+            roof = !roof;
+        } else if order % 2 == 0 {
+            engine = Transmission::SemiAuto;
+        } else {
+            engine = Transmission::Manual;
+            orders.insert("Manual".to_string(), manual);
+            manual += 1;
+        }
+
 
         // Order the cars, New are even numbers, Used are odd numbers
         // TO DO: Fix indexing into `colors` array, vary color for the orders
         if index % 2 != 0 {
-            car = car_factory(String::from(colors[index -1]), engine, roof, miles);
+            car = car_factory(String::from(colors[index - 1]), engine, roof, miles);
+            orders.insert(String::from("Used"), used_cars);
+            used_cars = used_cars + 1;
         } else {
-            car = car_factory(String::from(colors[index -1]), engine, roof, 0);
+            car = car_factory(String::from(colors[index - 1]), engine, roof, 0);
+            orders.insert(String::from("New"), new_cars);
+            new_cars = new_cars + 1;
         }
 
         // Display car order details
-        println!("{}: {}, Closed roof, {:?}, {}, {} miles", order, car.age.0, car.motor, car.color, car.age.1);
-
+        if car.roof && car.age.0 == "Used" {
+            println!("{}: {}, {:?}, Closed roof, {}, {} miles", order, car.age.0, car.motor, car.color, car.age.1);
+        } else if car.age.0 == "New" && car.roof {
+            println!("{}: {}, {:?}, Closed roof, {}", order, car.age.0, car.motor, car.color);
+        } else if !car.roof && car.age.0 == "Used" {
+            println!("{}: {}, {:?}, Convertible, {}, {} miles", order, car.age.0, car.motor, car.color, car.age.1);
+        } else {
+            println!("{}: {}, {:?}, Convertible, {}", order, car.age.0, car.motor, car.color);
+        }
         // Change values for next loop
         // TO DO: Increment "order" by 1, and "miles" by 1,000
         order += 1;
@@ -105,5 +133,6 @@ fn main() {
             index = 1;
         }
     }
+    println!("\nCar orders: {:?}", orders);
 }
 
